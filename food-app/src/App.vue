b4e9d68c1ddab39ff1ddc7610ef294a946255a5e
Azizul Haque <azizul.softsw@gmail.com>
2022-05-20 23:28:49 +0600

    app.vue

--- food-app/src/App.vue
+++ food-app/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+    <ModalPopup header="The Popup is done" text="this is some modal content" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ModalPopup from './components/ModalPopup.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{ ModalPopup },
+  data(){
+    return {
+      title: 'My first vue App!'
+    }
   }
 }
 </script>
@@ -22,5 +26,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #2c3e50;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
